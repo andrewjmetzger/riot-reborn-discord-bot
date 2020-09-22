14403f74086d9cee3ef3a79b64bf4bc4be68fe79
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2020-09-22 14:10:09 -0400

    Update linux runners.

--- forever.py
+++ forever.py
@@ -4,6 +4,6 @@ import sys, logging, pathlib
 
 while True:
     logging.warning("Restarting bot.py")
-    p = Popen([ str(pathlib.Path.home()) + "/Projects/riot-reborn-discord-bot/bot_env/bin/python3", "bot.py"])
+    p = Popen([ "./bot_env/bin/python3", "bot.py"])
     p.wait()
 
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env zsh
-cd ~/Projects/riot-reborn-discord-bot/
+#!/usr/bin/env bash 
 mkdir ./.logs/
 ./bot_env/bin/python3 "forever.py" &> ./.logs/riot_$(date +%F_%T).log
 exit
